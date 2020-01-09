12e2125f6ca06b6aee8b46f7a64a0b4250c8e6de
Sebastian Wallkotter <sebastian@wallkoetter.net>
2020-01-09 09:20:17 +0100

    reorganized assets

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ rm -r ${PWD}/tmp/*
 
 # build Panda .sdf with Gripper attached
 cp -r ${PWD}/assets/panda_arm_hand ${PWD}/tmp
-docker run -v ${PWD}/assets/franka_description:/xacro osrf/ros:kinetic-desktop-full rosrun xacro xacro --inorder /xacro/robots/panda_arm_hand.urdf.xacro > ${PWD}/tmp/panda_hand_arm.urdf
+docker run -v ${PWD}/assets/panda_xacro:/xacro osrf/ros:kinetic-desktop-full rosrun xacro xacro --inorder /xacro/panda_arm_hand.urdf.xacro > ${PWD}/tmp/panda_hand_arm.urdf
 docker run -v ${PWD}/tmp:/urdf gazebo gz sdf -p /urdf/panda_hand_arm.urdf > ${PWD}/tmp/panda_arm_hand/model.sdf
 
 # build Panda .sdf with Gripper attached
 cp -r ${PWD}/assets/panda_arm ${PWD}/tmp
-docker run -v ${PWD}/assets/franka_description:/xacro osrf/ros:kinetic-desktop-full rosrun xacro xacro --inorder /xacro/robots/panda_arm.urdf.xacro > ${PWD}/tmp/panda_arm.urdf
+docker run -v ${PWD}/assets/panda_xacro:/xacro osrf/ros:kinetic-desktop-full rosrun xacro xacro --inorder /xacro/panda_arm.urdf.xacro > ${PWD}/tmp/panda_arm.urdf
 docker run -v ${PWD}/tmp:/urdf gazebo gz sdf -p /urdf/panda_arm.urdf > ${PWD}/tmp/panda_arm/model.sdf
 
 # Build the image
